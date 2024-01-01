502e8981f79c9bc985854dd6556537c7e66a2d9a
William Thompson <wmthmps1@gmail.com>
2023-12-31 18:14:46 -0600

    Initial Commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+		public WordSmith()
+		{
+		}
+
+		public bool IsAPalindrome(string word)
+		{
+			word = word.ToLower();
+			if (word[0] == word[word.Length - 1])
+			{
+				return true;
+			} else
+			{
+				return false;
+			}
+		}
+	}
+}
+
+/*
+ * In your WordSmith class, you will create a method (IsAPalindrome) that 
+ * will take a parameter of type string and return a value of type bool. 
+ * If the given input is a palindrome – return true, else return false. 
+ * You will finish this method after you write your tests.
+ */
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,25 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("racecar", true)] // ← you will need to put some test data and expected result here.
+        [InlineData("not", false)]
+        [InlineData("Hannah", true)]
+        [InlineData("leveL", true)]
 
+        // For example [InlineData(“racecar”, true)]
+        public void Test1(string word, bool expected)
+        {
+            var wordSmith = new WordSmith();
+
+            var actual = wordSmith.IsAPalindrome(word);
+
+            Assert.Equal(actual, expected);
         }
     }
 }
